23e23578c88947c04eff1e02ce0f756a5ab64e5f
pljay <pljay@outlook.com>
2018-01-18 18:04:26 +0800

    delete android Change-Id: I57de84de27f91a1c8d5a095aed1e0ff45786fc1f Signed-off-by: pljay <pljay@outlook.com>

--- src/cn/wizool/android/or/manager/CDCEventManager.java
+++
@@ -1,7 +0,0 @@
-package cn.wizool.android.or.manager;
-import cn.wizool.android.or.CDCEvent;
-import java.util.concurrent.ConcurrentLinkedDeque;
-
-public class CDCEventManager {
-    public static final ConcurrentLinkedDeque<CDCEvent> queue = new ConcurrentLinkedDeque<>();
-}
